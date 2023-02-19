5ee8222bd5dbe57f7899c52f131f906c83f48f03
TesfaM <47814231+TesfaM@users.noreply.github.com>
2023-02-19 15:22:01 +0300

    log added

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ async function generateResponse(text) {
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
+    console.log(response.data.choices[0].text);
     return response.data.choices[0].text;
     // return "ተበዳ mars".response.data.choices[0].text;
     // return response.data.choices[0].text;
